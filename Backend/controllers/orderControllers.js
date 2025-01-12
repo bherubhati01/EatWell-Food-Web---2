765f30ab5ee8936710a8d2d12c4433c03ae26298
Bheru Bhati <bherubhati01@gmail.com>
2025-01-12 14:36:58 +0530

    Project Completed

--- Backend/controllers/orderControllers.js
+++ Backend/controllers/orderControllers.js
@@ -36,7 +36,7 @@ const placeOrder = async (req, res) => {
                 product_data: {
                     name: "Delivery Charges"
                 },
-                unit_amount: 2 * 100,
+                unit_amount: 2 * 100 *80,
             },
             quantity: 1
         })
