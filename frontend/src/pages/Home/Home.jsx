be8d07894224defd3d79963370e2412655193836
Bheru Bhati <bherubhati01@gmail.com>
2025-01-14 11:18:23 +0530

    Cart Button added on buttom

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import { useContext, useState } from 'react'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import Header from '../../components/Header/Header'
 import './Home.css'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
+import { assets } from '../../assets/assets'
+import {Link} from 'react-router-dom'
+import { StoreContext } from '../../Context/StoreContext';
 
 function Home() {
-
+  
+  const { getTotalCartAmount } = useContext(StoreContext)
   let [category, setCategory] = useState("All")
 
   return (
@@ -14,6 +18,7 @@ function Home() {
       <Header/>
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category}></FoodDisplay>
+      {getTotalCartAmount()>0 && <Link className='flot-Cart' to="/cart"><img src={assets.basket_icon} alt="" /></Link>}
       <AppDownload/>
     </div>
   )
